6d601864faefd5bbccbe34ba755817227511161b
R3X7 <dthinh3005@gmail.com>
2022-09-28 13:15:40 +0700

    dthinh update

---
+++ DTO/BillDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanDienThoai.DTO
+{
+    public class BillDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public DateTime CreateTime { get; set; }
+        public int Total { get; set; }
+    }
+}
---
+++ DTO/DetailBillDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanDienThoai.DTO
+{
+    public class DetailBillDTO
+    {
+        public int Id { get; set; }
+        public int BillId { get; set; }
+        public int ProDuctID { get; set; }
+        public string NameProduct{ get; set; }
+        public int Price{ get; set; }
+        public int Amount{ get; set; }
+        public int Total { get; set; }
+        
+    }
+}
