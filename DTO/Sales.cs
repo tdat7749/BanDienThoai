4226e1365a040876771945a860025d8f0a855f4c
tdat7749 <hyperiondat@gmail.com>
2022-10-05 20:50:41 +0700

    Thien Dat update cua Thien Thi

---
+++ DTO/Sales.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appp11
+{
+    internal class Sales
+    {
+        private int id;
+        private string nameSale;
+        private DateTime timeStart;
+        private DateTime timeEnd;
+
+
+        public Sales() { }
+
+        public Sales(int id, string nameSale, DateTime timeStart, DateTime timeEnd)
+        {
+            this.Id = id;
+            this.NameSale = nameSale;
+            this.TimeStart = timeStart;
+            this.TimeEnd = timeEnd;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string NameSale { get => nameSale; set => nameSale = value; }
+        public DateTime TimeStart { get => timeStart; set => timeStart = value; }
+        public DateTime TimeEnd { get => timeEnd; set => timeEnd = value; }
+    }
+}
---
+++ DTO/Supplier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appp11
+{
+    internal class Supplier
+    {
+        private int id;
+        private string nameSupplier;
+        private string addressSupplier;
+
+
+        public Supplier() { }
+        public Supplier(int id, string nameSupplier, string addressSupplier)
+        {
+            this.Id = id;
+            this.NameSupplier = nameSupplier;
+            this.AddressSupplier = addressSupplier;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string NameSupplier { get => nameSupplier; set => nameSupplier = value; }
+        public string AddressSupplier { get => addressSupplier; set => addressSupplier = value; }
+    }
+}
