1e95832abe8bca04868299bc56094564c48d5896
R3X7 <dthinh3005@gmail.com>
2022-10-11 22:50:14 +0700

    dthinh update

---
+++ BUS/BillBUS.cs
@@ -0,0 +1,36 @@
+﻿using BanDienThoai.DAO;
+using BanDienThoai.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanDienThoai.BUS
+{
+    internal class PermissionBUS
+    {
+        public static DataTable GetAllPermiss()
+        {
+            return PermissionDAO.GetAllPermiss();
+        }
+
+        [Obsolete]
+        public static void CreatePermiss(Permission per)
+        {
+            PermissionDAO.CreatePermiss(per);
+        }
+
+        [Obsolete]
+        public static void UpdatePermiss(Permission per)
+        {
+            PermissionDAO.UpdatePermiss(per);
+        }
+
+        public static void DeletePermiss(int ma)
+        {
+            PermissionDAO.DetelePermiss(ma);
+        }
+    }
+}
